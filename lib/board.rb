f92d36955631bd6cd06370f508dd753344800f64
YesSeri <henrik.zenkert@gmail.com>
2020-04-25 13:33:01 +0200

    Only legal moves for bishop, queen and rook allowed.

--- lib/board.rb
+++ lib/board.rb
@@ -18,22 +18,22 @@ class Board
   end
 
   def add_back_rank
-    @positions[0][0] = Rook.new(@black_player.color)
-    @positions[0][7] = Rook.new(@black_player.color)
+    @positions[0][0] = Rook.  new(@black_player.color)
+    @positions[0][7] = Rook.  new(@black_player.color)
     @positions[0][1] = Knight.new(@black_player.color)
     @positions[0][6] = Knight.new(@black_player.color)
     @positions[0][2] = Bishop.new(@black_player.color)
     @positions[0][5] = Bishop.new(@black_player.color)
-    @positions[0][3] = Queen.new(@black_player.color)
-    @positions[0][4] = King.new(@black_player.color)
-    @positions[7][0] = Rook.new(@white_player.color)
-    @positions[7][7] = Rook.new(@white_player.color)
+    @positions[0][3] = Queen. new(@black_player.color)
+    @positions[0][4] = King.  new(@black_player.color)
+    @positions[7][0] = Rook.  new(@white_player.color)
+    @positions[7][7] = Rook.  new(@white_player.color)
     @positions[7][1] = Knight.new(@white_player.color)
     @positions[7][6] = Knight.new(@white_player.color)
     @positions[7][2] = Bishop.new(@white_player.color)
     @positions[7][5] = Bishop.new(@white_player.color)
-    @positions[7][3] = Queen.new(@white_player.color)
-    @positions[7][4] = King.new(@white_player.color)
+    @positions[7][3] = Queen. new(@white_player.color)
+    @positions[7][4] = King.  new(@white_player.color)
   end
   def add_pawns
     8.times do |i|
@@ -53,14 +53,15 @@ class Board
       col = 7 
       start.push(row, col) 
       p start
-      finish_row = 6
+      finish_row = 2
       finish_col = 7 
       finish.push(finish_row, finish_col) 
       p finish
-      break if legal_move?(self, start, finish) #This is a module dedicated to finding legal moves 
+      #break if legal_move?(self, start, finish) #This is a module dedicated to finding legal moves 
+      break
   end
     @positions[finish_row][finish_col] = @positions[col][row]
-    @positions[col][row] = EMPTY_SQUARE
+    @positions[col][row] = Empty_Square.new
   end
   def to_s
     @positions.each do |row| 
@@ -74,5 +75,4 @@ end
 board = Board.new
 puts
 board.to_s
-board.make_move
-board.to_s
+print long_move_all_legal(board, [7, 3], [2, 0])
--- lib/rules.rb
+++ lib/rules.rb
@@ -1,30 +1,39 @@
 require 'pry'
 require 'pry-byebug'
 require_relative 'pieces/pieces.rb'
-
 module Rules
-  EMPTY_SQUARE = ".  "
- 
   def legal_move?(board, start, finish) #For squares, arrays, first value is row second is column.
     return false if test_range(start, finish) == false
     return false if start_square_legal?(board, start) == false
-    return false if end_square_legal?(board, start, finish) == false
-    return false if in_reach?(board, start, finish) == false
+    return false if finish_square_legal?(board, finish) == false
+    return false if pawn_capture_legal?(board, start, finish) == false
+    return false if piece_blocking?(board, start, finish) == true
     true
   end
 
   #checks if square is reachable
   #Different for different pieces. One for straight movers, one for knight and so on. 
 
-  def in_reach?(board, start, finish)
+  def piece_blocking?(board, start, finish)
+    piece = board.positions[start[0]][start[1]]
+    if piece.class == Knight || King #Knight jumps over, cant be blocked. King can always take. Control for check happens later. 
+      return true
+    elsif piece.class == Queen || Rook || Bishop
+      all_legal = long_move_all_legal(board, start, finish)
+    elsif piece.class == Pawn
+    end
     true
   end
+  def pawn_capture_legal?(board, start, finish)
   
+  end
   #Used for queen, bishop and rook. Returns all moves in bound. Doesnt notice if piece is in its way. Need to add a check if square is empty here, you can only move through empty squares, never through enemies. You can end up on enemy. 
   
-  def legal_moves_straight_lines(piece, position)
-    row = position[0]
-    column = position[1]
+  def long_move_all_legal(board, start, finish)
+    piece = board.positions[start[0]][start[1]]
+    empty_square = Empty_Square.new
+    row = start[0]
+    column = start[1]
     legal_moves = []
     piece.moveset.each do |move|
       temp_row = row
@@ -32,9 +41,14 @@ module Rules
       loop do
         temp_row += move[0]
         temp_col += move[1]
-        if temp_row.between?(0, 7) && temp_col.between?(0, 7)
+        break if !temp_row.between?(0, 7) || !temp_col.between?(0, 7)
+        # If a square has the same color piece as player he is blocked. 
+        if board.positions[temp_row][temp_col].color == board.current_player.color
+          break
+        elsif board.positions[temp_row][temp_col].color == empty_square.color 
           legal_moves << [temp_row, temp_col]
         else
+          legal_moves << [temp_row, temp_col]
           break
         end
       end
@@ -48,9 +62,9 @@ module Rules
     true
   end
 
-  def end_square_legal?(board, start, finish)
-    start_piece = board.positions[start[0]][start[1]]
+  def finish_square_legal?(board, finish)
     finish_piece = board.positions[finish[0]][finish[1]]
+    return false if finish_piece.color == board.current_player.color
     true
   end
 
@@ -65,5 +79,3 @@ module Rules
     square.all? { |a| true if a >= 0 && a <= 7 }
   end
 end
-
-include Rules
